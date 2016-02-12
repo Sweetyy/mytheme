0c7ba66bdb625f0694cb6d86b150719348d35ee4
Anthony <anthony.gallippi@safety-line.fr>
2016-02-12 14:48:25 +0100

    Adds mytheme

---
+++ sidebar.php
@@ -0,0 +1,7 @@
+<div class="sidebar"> 
+    <ul>
+        <li id="search"><?php include(TEMPLATEPATH . '/searchform.php'); ?></li>
+    </ul> 
+</div>
+	
+<?php wp_list_pages('title_li=<h2>Pages</h2>'); ?>
